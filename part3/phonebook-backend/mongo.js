0275b6bacf372c81d2b7889424d028159f1b67f3
Tomava <66073405+Tomava@users.noreply.github.com>
2022-09-16 14:52:38 +0300

    Add 3.12

---
+++ part3/phonebook-backend/mongo.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose')
+
+const url = process.env.MONGODB_URI
+
+const AddPerson = (name, phoneNumber) => {
+    mongoose.connect(url)
+    const person = new Person({
+        name: name,
+        number: phoneNumber,
+    })
+    person.save().then(result => {
+        console.log(`Added ${result.name} number ${result.number} to phonebook`)
+        mongoose.connection.close()
+    })
+}
+
+const PrintAll = () => {
+    mongoose.connect(url)
+    console.log('Phonebook:')
+    Person.find({}).then(result => {
+        result.forEach(person => {
+            console.log(`${person.name} ${person.number}`)
+        })
+        mongoose.connection.close()
+    })
+}
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (process.argv.length<3) {
+    console.log('Password is missing!')
+    process.exit(1)
+}
+const password = process.argv[2]
+
+if (process.argv.length === 3) {
+    PrintAll()
+}
+else if (process.argv.length === 5) {
+    const name = process.argv[3]
+    const phoneNumber = process.argv[4]
+    AddPerson(name, phoneNumber)
+}
+
+module.exports = mongoose.model('Person', personSchema)
