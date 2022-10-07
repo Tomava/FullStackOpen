bb7802a850ddb86ee6637efd319dcae340fa4a84
Tomava <66073405+Tomava@users.noreply.github.com>
2022-10-07 14:55:19 +0300

    Add 4.4 - 4.5

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,6 +2,18 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  return blogs.reduce((sum, blog) => sum + blog.likes, 0)
+}
+
+const favoriteBlog = (blogs) => {
+  return blogs.reduce((previous, current) => {
+    return previous.likes > current.likes ? previous : current
+  })
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes,
+  favoriteBlog
 }
