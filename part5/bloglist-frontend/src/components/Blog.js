d482f05bbbe21f01e0ab0e975975b16b8f77d98b
Tomava <66073405+Tomava@users.noreply.github.com>
2022-10-28 10:48:09 +0300

    Add 5.7

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,41 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+
+const Blog = ({blog}) => {
+  const [showDetails, setShowDetails] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const toggleDetails = () => {
+    setShowDetails(!showDetails)
+  }
+  
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author} <button onClick={toggleDetails}> {showDetails ? 'hide': 'show'} </button>
+      </div>
+      {showDetails &&
+        <div>
+          <div>
+          {blog.url}
+          </div>
+          <div>
+          likes {blog.likes}
+          </div>
+          <div>
+          {blog.user.name}
+          </div>
+        </div>
+      } 
+    </div>
+  )
+}
 
 export default Blog
