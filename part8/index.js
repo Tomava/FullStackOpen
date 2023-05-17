b6006c105fb22ac5350f2bc9f8d4b72bc1dc8598
Tomava <66073405+Tomava@users.noreply.github.com>
2023-05-17 14:14:03 +0300

    Add 8.7

--- part8/index.js
+++ part8/index.js
@@ -129,6 +129,11 @@ const typeDefs = `
       published: Int!,
       genres: [String!]!
     ): Book
+
+    editAuthor(
+      name: String!
+      setBornTo: Int!
+    ): Author
   }
 `
 
@@ -159,6 +164,15 @@ const resolvers = {
         authors = authors.concat(author)
       }
       return book
+    },
+    editAuthor: (root, args) => {
+      const author = authors.find((author) => author.name === args.name)
+      if (!author) {
+        return null
+      }
+      const updatedAuthor = {...author, born: args.setBornTo}
+      authors = authors.map((author) => author.name === args.name ? updatedAuthor : author)
+      return updatedAuthor
     }
   }
 }
