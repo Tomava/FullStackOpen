378771494490d2f1053ee6bfcbd43e9bb88f68d5
Tomava <66073405+Tomava@users.noreply.github.com>
2023-05-17 14:06:35 +0300

    Add 8.6

--- part8/index.js
+++ part8/index.js
@@ -1,5 +1,7 @@
 const { ApolloServer } = require('@apollo/server')
 const { startStandaloneServer } = require('@apollo/server/standalone')
+const { v1: uuid } = require('uuid')
+
 
 let authors = [
   {
@@ -119,6 +121,15 @@ const typeDefs = `
     allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
   }
+
+  type Mutation {
+    addBook(
+      title: String!,
+      author: String!,
+      published: Int!,
+      genres: [String!]!
+    ): Book
+  }
 `
 
 const resolvers = {
@@ -137,6 +148,17 @@ const resolvers = {
     },
     allAuthors: () => {
       return authors.map((author) => ({...author, bookCount: books.filter((book) => book.author === author.name).length}))
+    }
+  },
+  Mutation: {
+    addBook: (root, args) => {
+      const book = {...args, id: uuid()}
+      books = books.concat(book)
+      if (!authors.find((author) => author.name === args.author)) {
+        const author = {name: args.author, id: uuid()}
+        authors = authors.concat(author)
+      }
+      return book
     }
   }
 }
