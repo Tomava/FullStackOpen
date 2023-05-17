a26061cabae57d0e3d4523545b1ab5233aeee6f8
Tomava <66073405+Tomava@users.noreply.github.com>
2023-05-17 12:27:45 +0300

    Add 8.2

--- part8/index.js
+++ part8/index.js
@@ -98,16 +98,34 @@ let books = [
 */
 
 const typeDefs = `
+  type Author {
+    name: String!
+    id: ID!
+    born: Int!
+  }
+
+  type Book {
+    title: String!
+    author: String!
+    published: Int!
+    genres: [String!]!
+    id: ID!
+  }
+
   type Query {
-    bookCount: Int!,
+    bookCount: Int!
     authorCount: Int!
+    allBooks: [Book!]!
   }
 `
 
 const resolvers = {
   Query: {
     bookCount: () => books.length,
-    authorCount: () => authors.length
+    authorCount: () => authors.length,
+    allBooks: () => {
+     return books
+    }
   }
 }
 
