ccde4d031415ae7567ca7cf4e301e4f1b7163267
Tomava <66073405+Tomava@users.noreply.github.com>
2023-05-24 10:28:34 +0300

    Add 9.21 initials

---
+++ part9/patientor-frontend/src/types.ts
@@ -0,0 +1,30 @@
+export interface Diagnosis {
+  code: string;
+  name: string;
+  latin?: string;
+}
+
+export enum Gender {
+  Male = "male",
+  Female = "female",
+  Other = "other"
+}
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface Entry {
+
+}
+
+export interface Patient {
+  id: string;
+  name: string;
+  occupation: string;
+  gender: Gender;
+  ssn?: string;
+  dateOfBirth?: string;
+  entries: Entry[];
+}
+
+export type PatientFormValues = Omit<Patient, "id" | "entries">;
+
+export type NonSensitivePatient = Omit<Patient, "ssn" | "entries">;
